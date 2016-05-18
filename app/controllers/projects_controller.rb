edc06984d1f079a380f50d7a110bb5be6c1158bc
Brian Bier <briansbier@gmail.com>
2016-05-18 18:07:10 -0400

    Include links for when user sign in to create either a project or article

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
   before_action :find_project, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   def index
-    @projects = Project.all.order("created_at DESC")
+    @projects = Project.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 8)
   end
 
   def new

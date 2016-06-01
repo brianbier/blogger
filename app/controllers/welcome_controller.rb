bba4c17dcfe5b1c19be0fa1d8fe2279e5b86c044
Brian Bier <briansbier@gmail.com>
2016-06-01 13:43:45 -0400

    Style the new headline and add proper content

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @post = Post.all.limit(5).order("created_at DESC")
-    @projects = Project.all.limit(5).order("created_at DESC")
+    @post = Post.all.limit(10).order("created_at DESC")
+    @projects = Project.all.limit(10).order("created_at DESC")
   end
 end

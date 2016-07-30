6cf6868a104d8c8f9bedbabc625d74bd045157bb
Brian Bier <briansbier@gmail.com>
2016-07-29 23:53:48 -0400

    Add waypoints

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -7,3 +7,31 @@ $(document).ready(function(){
   });
 
 });
+
+/*********** WAYPOINTS ******************/
+$(document).ready(function(){
+  $(".wp1,.wp2,.wp3,.wp4").css('opacity', 0);
+
+
+  $(".wp1").waypoint(function () {
+    $(".wp1").addClass("animated fadeInUp ");
+  }, {
+    offset: "80%"
+  });
+  $(".wp2").waypoint(function () {
+    $(".wp2").addClass("animated fadeInUp ");
+  }, {
+    offset: "70%"
+  });
+  $(".wp3").waypoint(function () {
+    $(".wp3").addClass("animated fadeInUp ");
+  }, {
+    offset: "60%"
+  });
+  $(".wp4").waypoint(function () {
+    $(".wp4").addClass("animated fadeInUp ");
+  }, {
+    offset: "70%"
+  });
+});
+
